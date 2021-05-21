5a0639281c63a023f579cfd497bc7659c3424555
izabelsoares <izabelsoaresbds@gmail.com>
2021-05-21 20:27:21 -0300

    tarefa do object e array

--- script.js
+++ script.js
@@ -18,5 +18,33 @@ let cadastro = [
         cellnumber: 1234-5678,
         friends: ["A", "B", "C", "D"]
     },
+    {
+        name: "Carlos",
+        age: 19,
+        cellnumber: 5678-1234,
+        friends: ["E", "F", "G", "H"]
+    },
+    {
+        name: "João",
+        age: 21,
+        cellnumber: 8765-4321,
+        friends: ["I", "J", "K", "L"],
+    },
+    {
+        name: "Maria",
+        age: 17,
+        cellnumber: 4321-8765,
+        friends: ["M", "N", "O", "P"],
+    },
+    {
+        name: "Julia",
+        age: 15,
+        cellnumber: 1526-3748,
+        friends: ["Q", "R", "S", "T", "U"]
+    }
 ]
-console.log(cadastro[0].friends[3])
+console.log(cadastro[0].friends[2])
+console.log(cadastro[1].friends[1])
+console.log(cadastro[2].friends[0])
+console.log(cadastro[3].friends[3])
+console.log(cadastro[4].friends[4])
